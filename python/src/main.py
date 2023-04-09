27ebd16a46eb1c11a52330210aaa5e9538f701a3
Stephen Rayner <stephen.r.rayner@gmail.com>
2023-04-09 11:23:30 +0100

    build: add linters

--- python/src/main.py
+++ python/src/main.py
@@ -1,14 +1,18 @@
 """Main module"""
 
+import os
+
 import openai
+from dotenv import load_dotenv
 
 
 def main():
     """Main function"""
-    print("Hello World")
 
-    # Replace 'your_api_key' with your actual API key
-    openai.api_key = "your_api_key"
+    load_dotenv()
+
+    # Read API key from environment variable
+    openai.api_key = os.getenv("OPENAI_API_KEY")
 
     # Set up the API request parameters
     prompt = "Once upon a time in a land far, far away..."
@@ -23,7 +27,7 @@ def main():
         temperature=temperature,
         n=1,
         stop=None,
-        echo=False
+        echo=False,
     )
 
     # Extract and print the generated text

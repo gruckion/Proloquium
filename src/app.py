63b3291a2faa9b9e1006928d7cce37be0d2ce397
Stephen Rayner <stephen.r.rayner@gmail.com>
2023-06-04 17:23:09 +0100

    tweaks

--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@
 
 import chainlit as cl
 from dotenv import load_dotenv
-from langchain import LLMMathChain, OpenAI, SerpAPIWrapper
+from langchain import LLMChain, LLMMathChain, OpenAI, PromptTemplate, SerpAPIWrapper
 from langchain.agents import Tool, initialize_agent
 from langchain.chat_models import ChatOpenAI
 
@@ -14,10 +14,17 @@ Answer: Let's think step by step."""
 
 @cl.langchain_factory
 def factory():
-    llm = ChatOpenAI(temperature=0)
-    llm1 = OpenAI(temperature=0)
+    llm = OpenAI(temperature=0, streaming=True)
+    flag = True
+
+    if flag:
+        prompt = PromptTemplate(template=template, input_variables=["question"])
+        return LLMChain(prompt=prompt, llm=llm, verbose=True)
+
+
+    chat_llm = ChatOpenAI(temperature=0)
     search = SerpAPIWrapper()
-    llm_math_chain = LLMMathChain.from_llm(llm=llm, verbose=True)
+    llm_math_chain = LLMMathChain.from_llm(llm=chat_llm, verbose=True)
 
     tools = [
         Tool(
@@ -32,7 +39,7 @@ def factory():
         ),
     ]
     return initialize_agent(
-        tools, llm1, agent="chat-zero-shot-react-description", verbose=True
+        tools, llm, agent="chat-zero-shot-react-description", verbose=True
     )
 
 @cl.action_callback("action_button")
@@ -51,9 +58,30 @@ def on_action(action):
 
 @cl.on_chat_start
 def start():
+    pass
     # Sending an action button within a chatbot message
-    actions = [
-        cl.Action(name="action_button", value="example_value", description="Click me!")
-    ]
+    # actions = [
+    #     cl.Action(name="action_button", value="example_value", description="Click me!")
+    # ]
 
-    cl.Message(content="Interact with this action button:", actions=actions).send()
+    # cl.Message(content="Interact with this action button:", actions=actions).send()
+
+    # res = cl.AskUserMessage(content="What is your name?", timeout=10).send()
+    # if res:
+    #     cl.Message(
+    #         content=f"Your name is: {res['content']}",
+    #     ).send()
+
+
+    # # Wait for the user to upload a file
+    # while file is None:
+    #     file = cl.AskFileMessage(
+    #         content="Please upload a text file to begin!", accept=["*"]
+    #     ).send()
+    # # Decode the file
+    # text = file.content.decode("utf-8")
+
+    # # Let the user know that the system is ready
+    # cl.Message(
+    #     content=f"`{file.name}` uploaded, it contains {len(text)} characters!"
+    # ).send()
